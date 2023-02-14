80289e8806df4c5f89eacca5bfc6e2b7ce42e2bd
ZippyDaFish <andrew.riker21@gmail.com>
2023-02-14 15:38:58 -0500

    Fix text display for tasks

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     super();
 
     this.state = {
-      task: { text: ''},
+      task: {
+        text: '', 
+        id: uniqid()
+      },
       tasks: [],
-      id: uniqid()
     };
   }
 
@@ -18,7 +20,7 @@ class App extends Component {
       task: {
         text: e.target.value,
         id: this.state.task.id,
-      }
+      },
     });
   };
 
@@ -40,7 +42,12 @@ class App extends Component {
       <div>
         <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter Task</label>
-          <input onChange={this.handleChange} type="text" id="taskInput"/>
+          <input 
+            onChange={this.handleChange} 
+            value={task.text} 
+            type="text" 
+            id="taskInput"
+          />
           <button type="submit">Add Task</button>
         </form>
         <Overview tasks={tasks}/>
