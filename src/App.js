60219789b28910c5efcb83f338692b91bf7f7001
ZippyDaFish <andrew.riker21@gmail.com>
2023-02-14 15:19:52 -0500

    Add list mapping

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      task: { text: ''},
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {text: ''},
+    });
+  };
+
   render(){
-    return <div>Hello World</div>;
+    const {task, tasks} = this.state;
+
+    return(
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter Task</label>
+          <input onChange={this.handleChange} type="text" id="taskInput"/>
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    )
   }
 }
 
